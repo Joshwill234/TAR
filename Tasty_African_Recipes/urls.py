c049e5a1d640687f1762d6f01f9070c9cd894b4b
joshwill234 <234joshwill@gmail.com>
2025-03-18 14:37:06 +0000

    header created and partly styled

--- Tasty_African_Recipes/urls.py
+++ Tasty_African_Recipes/urls.py
@@ -2,5 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.home, name='home.html'),
+    path('', views.signup, name='signup.html'),
+    path('', views.blog, name='blog.html'),
+    path('', views.login, name='login.html'),
+    path('', views.search, name='search.html'),
 ]
--- Tasty_African_Recipes/views.py
+++ Tasty_African_Recipes/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
-def index(request):
-    return render(request, "Tasty_African_Recipes/index.html")
+def home(request):
+    return render(request, "Tasty_African_Recipes/home.html")
 
+def signup(request):
+    return render(request, "Tasty_African_Recipes/signup.html")
+
+def login(request):    
+    return render(request, "Tasty_African_Recipes/login.html")
+
+def blog(request):
+    return render(request, "Tasty_African_Recipes/blog.html")
+
+def search(request):
+    return render(request, "Tasty_African_Recipes/search.html")
